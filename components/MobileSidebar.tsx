a733c30908b3787ed4714287974c7aac2a87fa8f
antwi robert <roberttantwi@outlook.com>
2025-11-20 00:55:42 +0000

    pdate project name and references to TripControl

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -16,7 +16,7 @@ const MobileSidebar = () => {
         <header>
             <Link to="/">
                 <img src="/assets/icons/logo.svg" alt="Logo" className="size-[30px]" />
-                <h1>Tourvisto</h1>
+                <h1>TripControl</h1>
             </Link>
 
             <button onClick={toggleSidebar}>
